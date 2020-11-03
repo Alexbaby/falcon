7fa2a1c50fe85998eeb4f550728d1e8a6e9d7f83
nintriva <qa.nintriva@gmail.com>
2020-11-03 12:58:21 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FalconSearchPageComponent } from './falcon-search-page/falcon-search-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component:FalconSearchPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
